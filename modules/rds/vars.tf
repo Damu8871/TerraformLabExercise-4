81bb2b6b86a858f2585b0dca4987563691a0d85f
Damodaran R <damodaran_r@outlook.com>
2018-06-26 13:46:31 +0530

    Create vars.tf

---
+++ modules/rds/vars.tf
@@ -0,0 +1,8 @@
+variable "security-group-id" {
+	default = "sg-0388724a4745facfb"
+}
+
+variable "subnet-ids" {
+	type = "list"
+	default = ["subnet-0c1284c38971c3628","subnet-04d38421520bd8e59"]
+}
